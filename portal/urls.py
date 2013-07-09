979037e591df35b39f3b618b761c8001162aa48e
Alexander Chen <alexwchen@Alexanders-MacBook-Pro.local>
2013-07-09 03:02:25 -0400

    url forms

--- portal/urls.py
+++ portal/urls.py
@@ -6,5 +6,8 @@ urlpatterns = patterns('',
     # Main web portal entrance.
     (r'^$', portal_main_page),
     (r'^test$', user_test),
+    (r'^contact$', contact),
+    (r'^register$', register),
+
 
 )
